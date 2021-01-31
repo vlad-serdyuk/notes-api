51a256df7b26092744014499b8b648b046c83713
vlad.serdyuk <vladserduk@live.com>
2021-01-31 20:23:15 +0100

    Add favoritedBy for comment query

--- src/resolvers/comment.js
+++ src/resolvers/comment.js
@@ -2,4 +2,5 @@ const UserService = require('../services/user-service');
 
 module.exports = {
   author: ({ author }) => UserService.getAuthor({ author }),
+  favoritedBy: ({ favoritedBy }) => UserService.getAuthorByFavoritedBy({ favoritedBy }),
 };
